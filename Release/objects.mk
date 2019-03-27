49f8c62f52320402885deac42cb205c5aaf2c101
Scoopta <scoopta@scoopta.ninja>
2019-03-27 01:25:33 -0700

    Replaced binary dependencies with source files

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lwl_protos -lwayland-client
+LIBS :=
 
---
+++ Release/proto/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../proto/wlr-screencopy-unstable-v1-protocol.c \
+../proto/xdg-output-unstable-v1-protocol.c 
+
+OBJS += \
+./proto/wlr-screencopy-unstable-v1-protocol.o \
+./proto/xdg-output-unstable-v1-protocol.o 
+
+C_DEPS += \
+./proto/wlr-screencopy-unstable-v1-protocol.d \
+./proto/xdg-output-unstable-v1-protocol.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+proto/%.o: ../proto/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I../inc -O3 -Wall -Wextra -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
